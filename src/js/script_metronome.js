a8d6c3506585a753085f5a0c9bcd95b18bffd723
TKunnapuu <tk.taavi@gmail.com>
2022-11-09 15:58:10 +0200

    Lisatud vahelehed, metronoomi uuendus

--- src/js/script_metronome.js
+++ src/js/script_metronome.js
@@ -1,17 +1,24 @@
-const metronome = new Audio('metronome.wav');
+const metronome = new Audio('./audio/metronome2.wav');
+var slider = document.getElementById('bpm');
 
 let playing = false;
 
 function play() {
   playing = true;
+  console.log(bpm);
   const interval = setInterval(() => {
     if (!playing) {
       clearInterval(interval);
     }
     metronome.play();
-  }, 1000);
+  }, 60000 / bpm);
 }
 
 function stop() {
   playing = false;
 }
+
+slider.oninput = function () {
+  bpm = this.value;
+  document.getElementById('bpm-value').innerHTML = bpm;
+};
