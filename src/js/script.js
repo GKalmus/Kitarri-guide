1d78e58ff032a73741da59d281687359c929d3ee
Taavi Künnapuu <tk.taavi@gmail.com>
2022-11-10 12:56:28 +0200

    JS menüüde peitmine

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 console.log('Hello World');
 
 window.onclick = (e) => {
-  console.log(e.target);
+  if (e.target.id === 'closeMenuBtn') {
+    if (
+      isHidden(
+        e.target.parentNode.parentNode.getElementsByClassName(
+          'article-content'
+        )[0]
+      )
+    ) {
+      e.target.style.transform = 'rotate(0deg)';
+      e.target.parentNode.parentNode.getElementsByClassName(
+        'article-content'
+      )[0].style.display = 'block';
+    } else {
+      e.target.style.transform = 'rotate(180deg)';
+      e.target.parentNode.parentNode.getElementsByClassName(
+        'article-content'
+      )[0].style.display = 'none';
+    }
+  }
 };
+
+function isHidden(el) {
+  var style = window.getComputedStyle(el);
+  return style.display === 'none';
+}
