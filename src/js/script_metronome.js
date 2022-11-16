c3421cadf5b89b6a029fb27cb9e035ee13ec5d01
TKunnapuu <39981691+TKunnapuu@users.noreply.github.com>
2022-11-16 21:37:33 +0200

    Uuendatud metronoom (mitte lõplik)

--- src/js/script_metronome.js
+++ src/js/script_metronome.js
@@ -2,15 +2,20 @@ const metronome = new Audio('./audio/metronome2.wav');
 var slider = document.getElementById('bpm');
 
 let playing = false;
+let wasPlaying = false;
+let cleared = false;
 
-function play() {
+async function play() {
   playing = true;
   console.log(bpm);
   const interval = setInterval(() => {
     if (!playing) {
       clearInterval(interval);
+      console.log('stopped');
+      cleared = true;
+    } else {
+      metronome.play();
     }
-    metronome.play();
   }, 60000 / bpm);
 }
 
@@ -18,7 +23,22 @@ function stop() {
   playing = false;
 }
 
+slider.onmousedown = function () {
+  wasPlaying = playing;
+  stop();
+};
+
 slider.oninput = function () {
   bpm = this.value;
+  playing = false;
   document.getElementById('bpm-value').innerHTML = bpm;
+};
+
+slider.onmouseup = function () {
+  if (wasPlaying && !cleared) {
+    console.log('Alsutas liiga ruttu, error');
+  } else if (wasPlaying && cleared) {
+    play();
+    cleared = false;
+  }
 };
